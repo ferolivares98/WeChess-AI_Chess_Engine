ddcac14bf7ec72b560c45f089cb7a5ffe3c35961
fernando <ferolinaranjo@gmail.com>
2021-10-04 18:55:53 +0200

    En passant + pin bug fix

--- game/tablero.py
+++ game/tablero.py
@@ -357,6 +357,7 @@ class Tablero:
                 break
 
         if self.turno_blancas:
+            k_fil, k_col = self.wKing
             if self.board[fil - 1][col] == "--":
                 if not pin_actual or pin_dir == (-1, 0):
                     lista_moves.append(Movimiento((fil, col), (fil - 1, col), self.board))
@@ -368,14 +369,51 @@ class Tablero:
                         lista_moves.append(Movimiento((fil, col), (fil - 1, col - 1), self.board))
                 elif (fil - 1, col - 1) == self.pos_posible_en_passant:
                     # Utilizamos el parámetro opcional
-                    lista_moves.append(Movimiento((fil, col), (fil - 1, col - 1), self.board, en_passant_posible=True))
+                    pieza_ataque = pieza_pin_o_bloqueo = False
+                    if k_fil == fil:
+                        if k_col < col:
+                            rango_interior = range(k_col + 1, col - 1)
+                            rango_exterior = range(col + 1, COLS)
+                        else:
+                            rango_interior = range(k_col - 1, col, -1)
+                            rango_exterior = range(col - 2, -1, -1)
+                        for i in rango_interior:
+                            if self.board[fil][i] != "--":  # ALguna pieza bloquea
+                                pieza_pin_o_bloqueo = True
+                        for i in rango_exterior:
+                            if self.board[fil][i][0] == 'b' and (self.board[fil][i][1] == 'R'
+                                                                 or self.board[fil][i][1] == 'Q'):
+                                pieza_ataque = True
+                            elif self.board[fil][i] != '--':
+                                pieza_pin_o_bloqueo = True
+                    if not pieza_ataque or pieza_pin_o_bloqueo:
+                        lista_moves.append(Movimiento((fil, col), (fil - 1, col - 1), self.board, en_passant_posible=True))
             if col + 1 <= 7:  # Captura derecha
                 if self.board[fil - 1][col + 1][0] == 'b':
                     if not pin_actual or pin_dir == (-1, 1):
                         lista_moves.append(Movimiento((fil, col), (fil - 1, col + 1), self.board))
                 elif (fil - 1, col + 1) == self.pos_posible_en_passant:
-                    lista_moves.append(Movimiento((fil, col), (fil - 1, col + 1), self.board, en_passant_posible=True))
+                    pieza_ataque = pieza_pin_o_bloqueo = False
+                    if k_fil == fil:
+                        if k_col < col:
+                            rango_interior = range(k_col + 1, col)
+                            rango_exterior = range(col + 2, COLS)
+                        else:
+                            rango_interior = range(k_col - 1, col + 1, -1)
+                            rango_exterior = range(col - 1, -1, -1)
+                        for i in rango_interior:
+                            if self.board[fil][i] != "--":  # ALguna pieza bloquea
+                                pieza_pin_o_bloqueo = True
+                        for i in rango_exterior:
+                            if self.board[fil][i][0] == 'b' and (self.board[fil][i][1] == 'R'
+                                                                 or self.board[fil][i][1] == 'Q'):
+                                pieza_ataque = True
+                            elif self.board[fil][i] != '--':
+                                pieza_pin_o_bloqueo = True
+                    if not pieza_ataque or pieza_pin_o_bloqueo:
+                        lista_moves.append(Movimiento((fil, col), (fil - 1, col + 1), self.board, en_passant_posible=True))
         else:
+            k_fil, k_col = self.bKing
             if self.board[fil + 1][col] == "--":
                 if not pin_actual or pin_dir == (1, 0):
                     lista_moves.append(Movimiento((fil, col), (fil + 1, col), self.board))
@@ -392,7 +430,25 @@ class Tablero:
                     if not pin_actual or pin_dir == (1, 1):
                         lista_moves.append(Movimiento((fil, col), (fil + 1, col + 1), self.board))
                 elif (fil + 1, col + 1) == self.pos_posible_en_passant:
-                    lista_moves.append(Movimiento((fil, col), (fil + 1, col + 1), self.board, en_passant_posible=True))
+                    pieza_ataque = pieza_pin_o_bloqueo = False
+                    if k_fil == fil:
+                        if k_col < col:
+                            rango_interior = range(k_col + 1, col - 1)
+                            rango_exterior = range(col + 1, COLS)
+                        else:
+                            rango_interior = range(k_col - 1, col - 1)
+                            rango_exterior = range(col - 2, -1, -1)
+                        for i in rango_interior:
+                            if self.board[fil][i] != "--":  # ALguna pieza bloquea
+                                pieza_pin_o_bloqueo = True
+                        for i in rango_exterior:
+                            if self.board[fil][i][0] == 'b' and (self.board[fil][i][1] == 'R'
+                                                                 or self.board[fil][i][1] == 'Q'):
+                                pieza_ataque = True
+                            elif self.board[fil][i] != '--':
+                                pieza_pin_o_bloqueo = True
+                    if not pieza_ataque or pieza_pin_o_bloqueo:
+                        lista_moves.append(Movimiento((fil, col), (fil + 1, col + 1), self.board, en_passant_posible=True))
         # Faltan promociones y en passant...
 
     def get_Rook_Mov(self, fil, col, lista_moves):
